2a85e1b74f8568c4078a7b7df8bf01fa8dadefaf
Денис Марьянский <d.maryanskiy@gmail.com>
2020-11-06 16:13:52 +0300

    ingredients are in db

--- foodgram/receipe/views.py
+++ foodgram/receipe/views.py
@@ -1,4 +1,15 @@
+import json
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from .models import Receipe
+from .models import Receipe, Ingredients
+
+def ingredients(request):
+    with open("ingredients.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+    for ingr in data:
+        ingredient = Ingredients(title=ingr["title"], dimension=ingr["dimension"])
+        ingredient.save()
+    return HttpResponse("\n".join(str(data)))
